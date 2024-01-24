1c311f35912326c9bbaba03d0eb1f3c8c470c222
Dattatreya Sengupta -X (dasengup) <dasengup@cisco.com>
2024-01-24 22:36:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { createRoot } from "react-dom/client";
+
+
+const App = () => {
+    return (
+        <>
+        <div>
+            Hello
+        </div>
+        </>
+        
+    )
+}
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(<App/>)
